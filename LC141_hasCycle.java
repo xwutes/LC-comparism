beae0ce5849864d6acd276f8dbf6e91d9e409ebb
xwutes <92950517+xwutes@users.noreply.github.com>
2022-05-22 11:52:28 -0400

    Initial commit

---
+++ LC141_hasCycle.java
@@ -0,0 +1,15 @@
+//LC141_Linkedlistcycle
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ LC_11 container with most water maxArea.java
@@ -0,0 +1,23 @@
+//LC_11 Container With Most Water maxArea
+
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1;
+        int res = 0;
+        while (left < right) {
+            // [left, right] 之间的矩形面积
+            int cur_area = Math.min(height[left], height[right]) * (right - left);
+            res = Math.max(res, cur_area);
+            // 双指针技巧，移动较低的一边
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return res;
+    }
+}
+// 详细解析参见：
+// https://labuladong.github.io/article/?qno=11
+
