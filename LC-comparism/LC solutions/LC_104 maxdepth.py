e9f4b9d71461331fbf8aa39819a26cb2d5a06e2e
xwutes <92950517+xwutes@users.noreply.github.com>
2022-06-27 13:45:06 -0400

    BS Tree iterative solution

--- LC-comparism/LC solutions/LC_104 maxdepth.py
+++ LC-comparism/LC solutions/LC_104 maxdepth.py
@@ -3,3 +3,25 @@ def maxDepth(root):
     if root is None:
         return 0
     return 1 + max(maxDepth(root.left), maxDepth(root.right))
+
+# ----------------- iterative way using BFS -------------------
+import collections
+
+
+def iter_maxDepth(root):
+    if not root:
+        return 0
+    q = collections.deque([root])
+    level = 0
+
+    while q:
+        for i in range(len(q)):
+            node = q.popleft()
+
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+        level += 1
+
+    return level
