8f6c4c3023b13e19efb8cc9f471a52b2dd1ed3de
xwutes <92950517+xwutes@users.noreply.github.com>
2022-06-27 23:01:52 -0400

    again using BFS to output node set

---
+++ LC-comparism/LC solutions/LC_226 InvertBTree.py
@@ -0,0 +1,43 @@
+# LC_226 InvertBTree
+# Definition for a binary tree node.
+from matplotlib import collections
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return None
+
+        q = collections.deque([root])
+
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                node.left, node.right = node.right, node.left
+
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+
+        return root
+
+# ------------------------- recursion way ----------------
+# this approach consumes much more RAM than the previous one.
+def invertTree(self, root: TreeNode) -> TreeNode:
+
+    if not root:
+        return None
+    
+    left = invertTree(root.left)            # recursion postion that function on tree left
+    right = invertTree(root.right)
+
+    root.right, root.left = left,right      # swap the value assigned before
+
+    return root
+
