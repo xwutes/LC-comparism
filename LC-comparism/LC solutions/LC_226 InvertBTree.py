aafc294d52b197ae1fa07d06882518127546bbff
xwutes <92950517+xwutes@users.noreply.github.com>
2022-06-29 13:52:06 -0400

    BFS comments adding

--- LC-comparism/LC solutions/LC_226 InvertBTree.py
+++ LC-comparism/LC solutions/LC_226 InvertBTree.py
@@ -19,13 +19,13 @@ class Solution:
             for i in range(len(q)):
                 node = q.popleft()
                 node.left, node.right = node.right, node.left
-
+                                                # swapping the pointer at this spot 
                 if node.left:
-                    q.append(node.left)
+                    q.append(node.left)         # almost same handling interms of append method
                 if node.right:
                     q.append(node.right)
 
-        return root
+        return root                             # the output should be an entire treenode, no counting involves
 
 # ------------------------- recursion way ----------------
 # this approach consumes much more RAM than the previous one.
