6dfa4d031644a0722105071a07de0d09c83fe3a8
xwutes <92950517+xwutes@users.noreply.github.com>
2022-07-12 12:38:11 -0400

    adding another solution, that using mid as agent

--- LC-comparism/LC solutions/LC_75 sort colors.py
+++ LC-comparism/LC solutions/LC_75 sort colors.py
@@ -17,3 +17,25 @@ class Solution:
             if nums[i] == 1:
                 nums[i], nums[ptrhead] = nums[ptrhead],nums[i]
                 ptrhead += 1
+
+
+# -------------------- faster and smarter solution ---------------------
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left, mid = 0
+        right = len(nums)-1
+
+        while mid <= right:
+            if nums[mid] == 0:
+                nums[left], nums[mid] = nums[mid], nums[left]
+                left += 1
+                mid += 1
+            
+            elif nums[mid] == 1:
+                mid += 1
+
+            else:
+                nums[mid], nums[right] = nums[right], nums[mid]
+                right -= 1
